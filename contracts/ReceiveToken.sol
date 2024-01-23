7fd6f560eda4b9d6ee4f49f36d1d9b823039d8dd
Lukina-Marina <lukina_marina12@mail.ru>
2024-01-23 12:31:40 +0700

    Added function addSubscription

--- contracts/ReceiveToken.sol
+++ contracts/ReceiveToken.sol
@@ -3,6 +3,19 @@ pragma solidity 0.8.19;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ReceiveToken {
+    struct Subscription {
+        uint256 amount;
+        uint256 subscriptionPeriod;
+        address reciever;
+        address token;
+    }
+    
+    Subscription[] public subscriptions;
+
+    function addSubscription(Subscription memory subscription) external {
+        subscriptions.push(subscription);
+    }
+
     function receiveTokens(address token, uint256 amount) external {
         address from = msg.sender;
         address to = address(this);
