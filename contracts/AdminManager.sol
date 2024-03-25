f1b67458a6afd88aa88e7db45c7d5366b0f9cdcb
Lukina-Marina <lukina_marina12@mail.ru>
2024-03-25 15:10:54 +0500

    Added functions to AdminManager contract

--- contracts/AdminManager.sol
+++ contracts/AdminManager.sol
@@ -3,12 +3,55 @@
 pragma solidity 0.8.19;
 
 import {IAdminManager} from "./interfaces/IAdminManager.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AdminManager is IAdminManager {
+contract AdminManager is IAdminManager, Ownable {
     //percentDenominator = 100_00 = 100%
     //amount * 1_00 / percentDenominator = 1% от amount
     //amount * 15_20 / percentDenominator = 15.2% от amount
-    uint256 public serviceFee = 200;
+    uint256 public serviceFee;
     address public feeReceiver;
     uint256 public extraGasAmount;
+    uint256 private constant PERCENT_DENOMINATOR = 10000;
+
+    constructor(uint256 _serviceFee, address _feeReceiver, uint256 _extraGasAmount) {
+        require(_serviceFee < PERCENT_DENOMINATOR, "AdminManager: Too big service fee");
+        require(_feeReceiver != address(0), "AdminManager: Zero fee receiver");
+        require(_extraGasAmount > 0, "AdminManager: Zero extra gas amount");
+
+        serviceFee = _serviceFee;
+        feeReceiver = _feeReceiver;
+        extraGasAmount = _extraGasAmount;
+
+        emit ServiceFeeChanged(0, _serviceFee);
+        emit FeeReceiverChanged(address(0), _feeReceiver);
+        emit ExtraGasAmountChanged(0, _extraGasAmount);
+    }
+
+    function setServiceFee(uint256 newServiceFee) external onlyOwner {
+        require(newServiceFee < PERCENT_DENOMINATOR, "AdminManager: Too big service fee");
+
+        uint256 oldValue = serviceFee;
+        serviceFee = newServiceFee;
+
+        emit ServiceFeeChanged(oldValue, newServiceFee);
+    }
+
+    function setFeeReceiver(address newFeeReceiver) external onlyOwner {
+        require(newFeeReceiver != address(0), "AdminManager: Zero fee receiver");
+
+        address oldValue = feeReceiver;
+        feeReceiver = newFeeReceiver;
+
+        emit FeeReceiverChanged(oldValue, newFeeReceiver);
+    }
+
+    function setExtraGasAmount(uint256 newExtraGasAmount) external onlyOwner {
+        require(newExtraGasAmount > 0, "AdminManager: Zero extra gas amount");
+
+        uint256 oldValue = extraGasAmount;
+        extraGasAmount = newExtraGasAmount;
+
+        emit ExtraGasAmountChanged(oldValue, newExtraGasAmount);
+    }
 }
--- contracts/interfaces/IAdminManager.sol
+++ contracts/interfaces/IAdminManager.sol
@@ -3,7 +3,14 @@
 pragma solidity 0.8.19;
 
 interface IAdminManager {
+    event ServiceFeeChanged(uint256 oldValue, uint256 newValue);
+    event FeeReceiverChanged(address oldValue, address newValue);
+    event ExtraGasAmountChanged(uint256 oldValue, uint256 newValue);
+
     function serviceFee() external view returns(uint256);
     function feeReceiver() external view returns(address);
     function extraGasAmount() external view returns(uint256);
+    function setServiceFee(uint256 newServiceFee) external;
+    function setFeeReceiver(address newFeeReceiver) external;
+    function setExtraGasAmount(uint256 newExtraGasAmount) external;
 }
